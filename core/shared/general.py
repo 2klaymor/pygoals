60a0c4a45a4bbc10a831dbfd6afac546252d4cf1
2klaymor <2klaymor@gmail.com>
2023-11-30 00:04:27 +0300

    .

--- core/shared/general.py
+++ core/shared/general.py
@@ -1,26 +1,33 @@
 APP_NAME = 'PyGoals'
 APP_VER = '0.1'
 
+
 CONFIG_PATH = 'config'
+CONFIG_FILE = 'config.json'
 DATA_PATH = 'data'
 DATA_FILE = 'data.db'
 
-CONFIG_FILE = 'config.json'
 
 BASE_CONFIG = {
     'lang': 'ru'
 }
 
+
 GOALS = 'Goals'
 TASKS = 'Tasks'
 
+
 ENCODING = 'utf-8'
+
+
+ICON = r'core\icon.ico'
+
 
 DEFAULT_WIDTH = 1024
 DEFAULT_HEIGHT = 768
-
 MIN_WIDTH = 640
 MIN_HEIGHT = 480
+
 
 LOCALE = None
 
